975a2f90dc1b68bd76d6eb1a13bdaf61aad51ce7
hiroto <kazamah@oregonstate.edu>
2021-05-31 18:32:56 -0700

    Implemented unittest method {implemented the program that matches the requiremnts for second test}

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,12 +5,13 @@ def fizzBuzz():
 
         if i%3 == 0:
             s += "Fizz "
-
-        elif i%3 != 0:
+        elif i%5 == 0:
+            s += "Buzz "
+        elif i%3 != 0 and i%5 != 0:
             s += str(i)
             s += " "
         i += 1
     return s
 
 
-#print(fizzBuzz())
+print(fizzBuzz())
